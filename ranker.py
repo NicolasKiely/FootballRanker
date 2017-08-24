8e3d5f3f1f37ccf35e1e6a46cd4868293ab704d1
nmkiely <nmkiely@ncsu.edu>
2017-08-24 13:18:59 -0400

    Allow using previous season's rankings as starting point

--- ranker.py
+++ ranker.py
@@ -5,17 +5,36 @@ import header.context
 import header.common
 
 
-if len(sys.argv) != 2:
-    print 'Usage:\n\tpython ranker.py [season]'
+argc = len(sys.argv)
+if argc != 2 and argc != 3:
+    print 'Usage:\n\tpython ranker.py [season] [optional previous season]'
     exit(0)
 
-# Load season
+# Load season(s)
 season_name = sys.argv[1]
-ctx = header.context.Context([season_name])
+if argc == 2:
+    ctx_seasons = [season_name]
+else:
+    prev_season_name = sys.argv[2]
+    ctx_seasons = [prev_season_name, season_name]
+
+ctx = header.context.Context(ctx_seasons)
 season = ctx.seasons[season_name]
 
+
 # Set initial scores
-initial_scores = {tname: 1.0 for tname, team in ctx.teams.iteritems()}
+if argc == 2:
+    # Just use 1.0
+    initial_scores = {tname: 1.0 for tname, team in ctx.teams.iteritems()}
+
+else:
+    prev_season = ctx.seasons[prev_season_name]
+    last_ranking = prev_season.weeks[-1].ranking
+
+    last_ranking.load()
+    initial_scores = last_ranking.score
+
+#initial_scores = {tname: 1.0 for tname, team in ctx.teams.iteritems()}
 
 # Calculate rankings
 season.calculate_rankings(initial_scores)
