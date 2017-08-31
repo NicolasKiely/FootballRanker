287c6f33afea718aa420ac4701da60e94287bae6
nmkiely <nmkiely@ncsu.edu>
2017-08-31 11:16:03 -0400

    Add formatting option for season report

--- picker.py
+++ picker.py
@@ -1,13 +1,43 @@
-''' Lists out season's picks '''
+''' Lists out season's picks for season. Add bb to use formatted code '''
 
 import sys
 import header.context
 import header.common
 
+plain_messages = {
+    'week_num': 'Week: %s',
+    'pick': 'Pick: %s',
+    'owned': 'Owned Teams:',
+    'score': 'Week Score: %s',
+    'proj_pre': '',
+    'proj_owned': 'Projected Owned Teams:',
+    'proj_score': 'Projected Week Score: %s',
+    'proj_post': '',
+    'begin': '',
+    'end': ''
+}
 
-if len(sys.argv) != 2:
+formatted_messages = {
+    'week_num': '[b]Week[/b]: %s',
+    'pick': '[u]Pick[/u]: %s',
+    'owned': '[u]Owned Teams[/u]:',
+    'score': '[u]Week Score[/u]: %s',
+    'proj_pre': '[color=gray]',
+    'proj_owned': '[u]Projected Owned Teams[/u]:',
+    'proj_score': '[u]Projected Week Score[/u]: %s',
+    'proj_post': '[/color]',
+    'begin': ' to lose.\n\n[font=Courier New][b]The Plan: Week [/b][/font]\n[spoiler]',
+    'end': '[/spoiler]'
+}
+
+argc = len(sys.argv)
+if argc != 2 and argc != 3:
     print 'Usage:\n\tpython picker.py [season]'
     exit(0)
+
+txt = formatted_messages if argc==3 else plain_messages
+
+
 
 # Load season
 season_name = sys.argv[1]
@@ -24,9 +54,10 @@ team_points = {t:0 for t in ctx.teams.keys()}
 proj_owned_teams = set()
 proj_team_points = {t:0 for t in ctx.teams.keys()}
 
+print txt['begin']
 for week in season.weeks:
     # For each week
-    print 'Week:', week.num
+    print txt['week_num'] % week.num
     all_matches_played = True
 
     # Load last standing ranking
@@ -69,7 +100,7 @@ for week in season.weeks:
                 
 
     # Evaluate best team pick
-    print '  Pick: '+ best_team,
+    print txt['pick'] % best_team,
     if best_match.played:
         if best_match.loser==best_team:
             print ' [Pick Aqcuired]'
@@ -86,20 +117,23 @@ for week in season.weeks:
         print
 
     # List team score status
-    print '  Owned Teams:\n    ',
+    print txt['owned'] +'\n    ',
     week_score = 0
     for team in owned_teams:
         week_score += team_points[team]
         print '%s [%s],' % (team, team_points[team]),
-    print '\n  Week Score: ', week_score
+    print '\n'+ txt['score']  % week_score
     print
     
     if not all_matches_played:
-        print '  Projected Owned Teams:\n    ',
+        print txt['proj_pre'],
+        print txt['proj_owned'] +'\n    ',
         proj_score = 0
         for team in proj_owned_teams:
             proj_score += proj_team_points[team]
             print '%s [%s],' % (team, proj_team_points[team]),
 
-        print '\n  Projected Week Score: ', proj_score
-        print
+        print '\n'+ txt['proj_score'] % proj_score
+        print txt['proj_post']
+
+print txt['end']
